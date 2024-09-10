172475f4f8f1820da5076dd4b5cc1d906d46329a
Shreethaar <shreethaara380@gmail.com>
2024-09-10 19:33:17 +0000

    [UHKFgQzmpsDWY]

---
+++ mal_process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Connecting to a remote server..."
+sleep 5
+FLAG="flag{process_memory_find}"
+echo $FLAG > /dev/null
+while true; do
+    echo "Running malicious tasks..."
+    sleep 60
+done
